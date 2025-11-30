5cf28a11edd40b170be1c875cda4dec732fe2d43
Tanvir-Ramim <ramimhossan14@gmail.com>
2025-11-30 23:53:13 +0600

    project setup complete

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+<>
+       <h1 className='text-red-600'>Vite +sdfsdfsdf React</h1>
+       <div className="text-3xl font-bold underline">
+        sdfdsfassdfdsfs
+       </div>
+</>
+    );
+};
+
+export default Home;
